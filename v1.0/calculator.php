60ce9c28cf98a1678b1f753b6e62ec0e43c2294c
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2020-10-28 22:43:23 -0400

    Add files via upload

---
+++ v1.0/calculator.php
@@ -0,0 +1,49 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	$num1 = $_GET['num1'];
+	$num2 = $_GET['num2'];
+	$operator = $_GET['operation'];
+	
+	echo($op);
+	exit();
+	if ($operator === "+" || $operator === "-" || $operator === "*" || $operator === "/" || $operator === "**") {
+		$n1 = floatval($num1);
+		$n2 = floatval($num2);
+		
+		$result = Null;
+		switch ($operator) {
+			case "+":
+				$result = $n1 + $n2;
+				break;
+			case "-":
+				$result = $n1 - $n2;
+				break;
+			case "*":
+				$result = $n1 * $n2;
+				break;
+			case "/":
+				$result = $n1 / $n2;
+				break;
+			case "**":
+				$result = $n1 ** $n2;
+				break;
+		}
+		
+		if ($result !== Null) {
+			echo $result;
+		} else {
+			http_response_code(500);
+		}
+		
+	} else {
+		http_response_code(400);
+		exit();
+	}
+	
+} else if {$ SERVER['REQUEST_METHOD'] == 'HEAD') {
+	exit()
+} else {
+	http_response_code(405);
+	exit();
+}
+?>
