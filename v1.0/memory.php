3079e2af5b9c2218bb6a02b83ed986adcb92375e
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2020-12-01 22:36:01 -0500

    Add files via upload

--- v1.0/memory.php
+++ v1.0/memory.php
@@ -24,15 +24,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	echo $_POST['val'];
-	if (array_key_exists('val', $_POST)) {
-		$val = $_POST['val'];
-		setcookie('mem', $val);
-		echo $val;
-		exit();
-	} else {
-		http_response_code(400);
-		exit();
-	}
+	// if (array_key_exists('val', $_POST)) {
+		// $val = $_POST['val'];
+		// setcookie('mem', $val);
+		// echo $val;
+		// exit();
+	// } else {
+		// http_response_code(400);
+		// exit();
+	// }
 
 } else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
 	$current = 0;
